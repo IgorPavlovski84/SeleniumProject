a8fcb05893b82c7145d28e767340bbd04842d015
IgorPavlovski <IgorPavlovski@users.noreply.github.com>
2021-02-02 19:05:56 +0100

    the login test (positive scenario) finished

--- src/test/java/tests/RegisterNewUserPageTest.java
+++ src/test/java/tests/RegisterNewUserPageTest.java
@@ -51,50 +51,27 @@ public class RegisterNewUserPageTest {
     @Test(priority=1)
     public void testRegisterNewUserPage () {
 
-        //Create RegisterNewUserPage object
         newUser = new RegisterNewUserPage(this.driver);
-
-        //Check the Mrs radio button for Title option
         newUser.setTitleAsMr();
-
         newUser.setFirstName("MyName");
-
         newUser.setLastName("MyLastName");
-
         newUser.setPassword("5char");
-
         newUser.setBirthDay("15");
-
         newUser.setBirthMonth("11");
-
         newUser.setBirthYear("1991");
-
         newUser.setNewsletter();
-
         newUser.setOffers();
-
         newUser.setCompany(" My Company Name");
-
         newUser.setAddress1("Main Street 123");
-
         newUser.setAddress2("Additional address info");
-
         newUser.setCity("My City");
-
         newUser.setCountry("21");
-
         newUser.setState("50");
-
         newUser.setPostalCode("56445");
-
         newUser.setAdditionalInfo("Some additional info to be added");
-
         newUser.setHomePhone("004915125752526");
-
         newUser.setMobilePhone("004915125752527");
-
         newUser.setAddressAlias("Alias for the address");
-
         newUser.registerNewUser();
 
         String userName = driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[1]/a/span")).getText();
