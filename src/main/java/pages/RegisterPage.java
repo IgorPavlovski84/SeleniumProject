4f5dc59d5974b6666d71071c0bac8650da1be8e6
IgorPavlovski84 <60329780+IgorPavlovski84@users.noreply.github.com>
2021-02-02 11:13:26 +0100

    changes

--- src/main/java/pages/RegisterPage.java
+++ src/main/java/pages/RegisterPage.java
@@ -1,8 +1,27 @@
 package pages;
 
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 
 public class RegisterPage {
 
+    WebDriver driver;
+
+    By emailField = By.xpath("//*[@id=\"email_create\"]");
+
+
+    public RegisterPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    //Enter email to create an new user
+
+    public void enterEmailToCreateNewUser(String newEmail){
+
+        driver.findElement(emailField).sendKeys(newEmail + Keys.ENTER);
+
+    }
 
 }
--- src/test/java/tests/RegisterPageTest.java
+++ src/test/java/tests/RegisterPageTest.java
@@ -1,29 +1,73 @@
 package tests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import pages.RegisterPage;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class RegisterPageTest {
 
+    String driverPath = "";
+
     WebDriver driver;
 
-    By homePageUserName = By.xpath("//table//tr[@class='heading3']");
+    RegisterPage regPage;
+
+    @BeforeTest
+
+    public void setup(){
+
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe" );
+
+        driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.get("http://automationpractice.com/index.php?controller=authentication&back=my-account");
+
+    }
+
+    @Test(priority=0)
+
+    public void testEnterEmailToCreateNewUser(){
+
+        //Create registration Page object
+
+        regPage = new RegisterPage(driver);
+
+        regPage.enterEmailToCreateNewUser("newemail@domain.com");
 
 
 
-    public Guru99HomePage(WebDriver driver){
+        Assert.assertEquals("Login - My Store", driver.getTitle());
 
-        this.driver = driver;
+        //Verify login page title
+
+        //String loginPageTitle = objLogin.getLoginTitle();
+
+        //Assert.assertTrue(loginPageTitle.toLowerCase().contains("guru99 bank"));
+
+        //login to application
+
+        //objLogin.loginToGuru99("mgr123", "mgr!23");
+
+        // go the next page
+
+        //objHomePage = new Guru99HomePage(driver);
+
+        //Verify home page
+
+        //Assert.assertTrue(objHomePage.getHomePageDashboardUserName().toLowerCase().contains("manger id : mgr123"));
 
     }
 
-    //Get the User name from Home Page
 
-    public String getHomePageDashboardUserName(){
-
-        return    driver.findElement(homePageUserName).getText();
-
-    }
 
 }
